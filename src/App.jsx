b4c980ffca56c213dc485752bd6020eb9f50b135
saezeric <saezescalonaeric@fpllefia.com>
2025-01-14 20:00:26 +0100

    Tarea 1: Instala React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./styles/bootstrap.scss";
 import "./styles/custom.scss";
 import { TablaPartidas } from "./Components/TablaPartidas";
