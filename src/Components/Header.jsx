cda71f652e7884570d35502b2d61b433dfe99e10
saezeric <saezescalonaeric@fpllefia.com>
2025-01-15 17:27:38 +0100

    Tarea 7: Menú de navegación añadido

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,34 @@
 export function Header() {
   return (
     <header className="d-flex flex-column align-items-center justify-content-center">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top w-100">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <a className="nav-link text-white" href="/">
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="partidas">
+                  Partidas
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="juego">
+                  Juego
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="ranking">
+                  Ranking
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <div className="mt-5 pt-5">
         <img
           src="../../public/img/logo.png"
