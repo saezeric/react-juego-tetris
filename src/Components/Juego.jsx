773f2a3b747c45d6e47853d059ed37ab54df3518
saezeric <saezescalonaeric@fpllefia.com>
2025-01-24 19:15:58 +0100

    Tarea 3: Componente pieza.jsx

--- src/Components/Juego.jsx
+++ src/Components/Juego.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Panel } from "./Panel";
+import { Pieza } from "./Pieza";
 import { modelos } from "../lib/modelos";
 import { useState } from "react";
 
@@ -7,6 +8,10 @@ export function Juego() {
   const [arrayCasillas, setArrayCasillas] = useState(modelos.matriz);
   return (
     <>
+      <Pieza modelo={modelos.piezas[0].matriz[0]} />
+      <Pieza modelo={modelos.piezas[1].matriz[0]} />
+      <Pieza modelo={modelos.piezas[2].matriz[0]} />
+      <Pieza modelo={modelos.piezas[3].matriz[0]} />
       <Panel modelo={arrayCasillas} />
     </>
   );
